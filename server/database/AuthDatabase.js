641ca822381e8a00f92ccc1630a34dc5d1b3df13
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-17 19:05:52 -0500

    Made create table statement run

--- server/database/AuthDatabase.js
+++ server/database/AuthDatabase.js
@@ -12,7 +12,7 @@ class AuthDatabase
             this.db = new Database('./server/database/AuthDatabase.sqlite3');
             this.db.pragma('journal_mode = WAL');
 
-            this.db.exec("CREATE TABLE IF NOT EXISTS auth (userid TEXT PRIMARY KEY, hash TEXT) ");
+            this.db.exec("CREATE TABLE IF NOT EXISTS auth (userid TEXT PRIMARY KEY, hash TEXT) ").run();
         }
         
     }
