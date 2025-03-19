379ba1d39d4f11f629bb9f78c05365630b17c833
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-18 20:57:06 -0500

    Implemented Signup API

---
+++ server/authentication/SignupInterface.js
@@ -0,0 +1,42 @@
+import AuthDatabase from "../database/AuthDatabase.js";
+import AppDatabase from "../database/AppDatabase.js";
+
+import crypto from 'crypto';
+
+class SignupInterface
+{   
+
+    static increment = 129471047261;
+
+    static salt = "salt" // Needs to be moved in enviroment file!!!!!!!!
+
+    static
+    {
+        // Retrieves the last user database to continue the increment if the server restarts 
+        let last = AuthDatabase.last_userid;
+        if (last)
+        {
+            this.increment = last + 2231;
+        }
+    }
+
+    static new_user(username, password)
+    {      
+        if (AppDatabase.username_exits(username) == 1 )
+        {
+            return -1;
+        }
+
+        let userid = String(this.increment);
+
+        this.increment += 2231
+        let hash = crypto.createHash('sha256', password+this.salt).digest('hex');;
+
+
+        AuthDatabase.add_auth(userid, hash)
+        AppDatabase.add_user(userid, username)
+        return 1;
+    }
+}
+
+export default SignupInterface;
--- server/authentication/signup.js
+++ server/authentication/signup.js
@@ -1,12 +1,37 @@
-import {express, Router} from 'express';
+import express from 'express';
+import SignupInterface from './SignupInterface.js'
 
-const signup = Router();
+const signup = express.Router();
 
 signup.use(express.json())
 
-signup.post('/signup', (req, res) => {
-  res.end();
-})
+signup.post('/', (req, res) => {
 
+  let query
+
+  try
+  {
+    query = SignupInterface.new_user(req.body.username, req.body.password)
+  }
+  catch (error)
+  {
+    console.log(error)
+    res.status(500).send("Internal Server Error")
+  }
+
+  switch (query) {
+    case 1:
+      res.set({'Content-Type': 'application/json' });
+      res.status(201)
+      res.send({ status : 'Created', message: '' })
+      break;
+    case -1:
+      res.set({'Content-Type': 'application/json' })
+      res.status(409)
+      res.send({ status : 'ERROR', message: 'Username already is in use.' })
+      break;
+}
+  
+})
 
 export default signup
