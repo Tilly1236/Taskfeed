2746e5b5ce497514898d36cc735b235c58aefec9
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-18 21:55:10 -0500

    indentation

--- server/authentication/signup.js
+++ server/authentication/signup.js
@@ -5,33 +5,34 @@ const signup = express.Router();
 
 signup.use(express.json())
 
-signup.post('/', (req, res) => {
+signup.post('/', (req, res) =>{
 
-  let query
+	let query
 
-  try
-  {
-    query = SignupInterface.new_user(req.body.username, req.body.password)
-  }
-  catch (error)
-  {
-    console.log(error)
-    res.status(500).send("Internal Server Error")
-  }
+	try
+	{
+		query = SignupInterface.new_user(req.body.username, req.body.password)
+	}
+	catch (error)
+	{
+		console.log(error)
+		res.status(500).send("Internal Server Error")
+	}
 
-  switch (query) {
-    case 1:
-      res.set({'Content-Type': 'application/json' });
-      res.status(201)
-      res.send({ status : 'Created', message: '' })
-      break;
-    case -1:
-      res.set({'Content-Type': 'application/json' })
-      res.status(409)
-      res.send({ status : 'ERROR', message: 'Username already is in use.' })
-      break;
-}
-  
+	switch (query)
+	{
+		case 1:
+			res.set({'Content-Type': 'application/json' });
+			res.status(201)
+			res.send({ status : 'Created', message: '' })
+			break;
+		case -1:
+			res.set({'Content-Type': 'application/json' })
+			res.status(409)
+			res.send({ status : 'ERROR', message: 'Username already is in use.' })
+			break;
+	}
+
 })
 
 export default signup
