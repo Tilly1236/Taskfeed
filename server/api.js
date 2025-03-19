d09d89d99c42ef7752e316e0e9c98e9d316f5a7d
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-18 21:45:08 -0500

    add login api route

--- server/api.js
+++ server/api.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
+
 import signup from './authentication/signup.js';
+import login from './authentication/login.js';
 
 const api = Router();
 
-api.use('/signup', signup)
+api.use('/signup', signup);
+api.use('/login', login);
 
 export default api;
 
