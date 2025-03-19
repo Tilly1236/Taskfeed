6150039a8263d9889e27e8c2bace2831e57753a4
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-18 21:47:31 -0500

    fixed username_exists name

--- server/database/AppDatabase.js
+++ server/database/AppDatabase.js
@@ -23,7 +23,7 @@ class AppDatabase
         this.db.prepare("INSERT INTO users VALUES (?, ?)").run(userid, username);
     }
 
-    static username_exits(username)
+    static username_exists(username)
     {
         return this.db.prepare("SELECT exists(SELECT 1 FROM users WHERE username = ?) AS row_exists;").get(username)['row_exists'];
     }
