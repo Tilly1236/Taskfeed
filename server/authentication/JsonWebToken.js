ecd41f18ef6d42e22d65d32e341458c6c34737f6
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-20 10:51:11 -0500

    Implemented error checking in the token parser

--- server/authentication/JsonWebToken.js
+++ server/authentication/JsonWebToken.js
@@ -3,6 +3,7 @@ import crypto from 'crypto'
 import process from 'process'
 
 import {Buffer} from 'node:buffer'
+import AuthError from '../errors/AuthError.js';
 
 class JsonWebToken
 {
@@ -16,6 +17,20 @@ class JsonWebToken
 		hmac.update(header);
 		hmac.update(payload);
 		return hmac.digest('hex');
+	}
+
+	static parse(token)
+	{
+		let split_token = token.split(".");
+
+		if (split_token.length != 3)
+		{
+			throw new AuthError("Invalid Token Format")
+		}
+
+		return split_token;
+
+
 	}
 
 	static create(payload)
@@ -33,11 +48,18 @@ class JsonWebToken
 
 	static verify(token)
 	{
-		let [header, payload, sig] = token.split(".");
+		let [header, payload, sig] = JsonWebToken.parse(token);
 
 		let verify_signature = JsonWebToken.signature(Buffer.from(header, "base64").toString("utf8"), Buffer.from(payload, "base64").toString("utf8"));
 
 		return (sig === verify_signature);
+	}
+
+	static data(token)
+	{
+		let [_header, payload, _sig] = JsonWebToken.parse(token)
+
+		return Buffer.from(payload, "base64").toString("utf8")
 	}
 }
 
