81072ec11d60ac7e2630be575e966092a3dd8f8c
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-20 10:51:37 -0500

    Implemented token verification

--- server/authentication/auth.js
+++ server/authentication/auth.js
@@ -1,13 +1,52 @@
 
 import JsonWebToken from './JsonWebToken.js';
+import AuthError from '../errors/AuthError.js';
 
 function auth() {
 	return function auth(req, res, next){
-		if (req) {
-		  let auth_header = req.get("Authorization")
+		
+		let verification
+		
+		if (!req)
+		{
+			return next();
 		}
-	
-		next();
+
+		let auth_header = req.get("Authorization");
+			
+		const [auth_scheme, token] = auth_header.split(" ");
+
+		if (auth_scheme != "Bearer")
+		{
+		return next(new AuthError("Invalid header"))
+		}
+
+		try
+		{
+			verification = JsonWebToken.verify(token);
+		} catch (error)
+		{
+			if (error instanceof AuthError)
+			{
+				return res.status(400).send("Invalid header");
+			}
+			else
+			{
+				console.log(error);
+				return res.status(500).send("Internal Server Error");
+			}
+		}
+
+		if (!verification)
+		{
+			return next(new AuthError("Invalid Token"))
+		}
+		
+		let payload = JsonWebToken.data(token);
+
+
+		console.log()
+		return next();
 	  };
   }
 
