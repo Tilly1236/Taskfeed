c9f4666f7aa954698ed3affcb55e75402d11f15c
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-03-22 16:48:33 -0500

    Removed auth middleware fomr server

--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ import api from './api.js'
 
 import process from 'process'
 
-import auth from './authentication/auth.js';
-
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/api', api);
-
-app.use(auth())
 
 app.get('/', (req, res) => {
   res.send("Hello World")
