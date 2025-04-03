d16be372d410437678bd1925a4f050b2d1643920
Tilly1236 <151090502+Tilly1236@users.noreply.github.com>
2025-04-03 02:12:20 -0500

    Update main.jsx
    Add AddPost page and route to project

--- client/main.jsx
+++ client/main.jsx
@@ -1,4 +1,4 @@
-import Adding from "./Adding.js";
+import AddPostPage from "./pages/AddPost.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
@@ -17,7 +17,7 @@ ReactDOM.createRoot(root).render(
       <Route path="/login" element={<Login />} />
       <Route path="/feed" element={<Feed />} />
       <Route path="/create_account" element={<CreateAccount />} />
-      <Route path="/add" element={<Adding />} />
+      <Route path="/add" element={<AddPostPage />} />
     </Routes>
   </BrowserRouter>
 );
