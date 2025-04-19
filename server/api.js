b33820553448900db3b619279e3b22b2ebe1d4f9
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-04-19 13:09:45 -0500

    Add endpoints for comment and commentfeed

--- server/api.js
+++ server/api.js
@@ -3,10 +3,14 @@ import {Router} from 'express';
 import post from './posts/post.js';
 import feed from './feed/feed.js';
 import filter from './feed/filter.js';
+import postcommment from './posts/postcomment.js';
+import commentfeed from './feed/commentfeed.js';
 
 const api = Router();
 
 api.use('/post', post); // Uses the router object defined by the token which handles requests at "/post"
+api.use('/comment', postcommment) // Uses the router object defined by the token which handles requests at "/comment"
+api.use('/commentfeed', commentfeed) // Uses the router object defined by the token which handles requests at "/comment"
 api.use('/feed', feed); // Uses the router object defined by the token which handles requests at "/feed"
 api.use(`/filter`, filter); // Uses the router object defined by the token which handles requests at "/filter"
 
