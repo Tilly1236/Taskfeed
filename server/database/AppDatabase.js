2533af63a03d844867552a930636e97082aa32b9
litegrandeur <98992279+litegrandeur@users.noreply.github.com>
2025-04-19 13:09:56 -0500

    Create comments table

--- server/database/AppDatabase.js
+++ server/database/AppDatabase.js
@@ -16,7 +16,9 @@ class AppDatabase
             this.db.prepare("CREATE TABLE IF NOT EXISTS users (userid INTEGER PRIMARY KEY, username TEXT UNIQUE) ").run();
 
             // posts
-            this.db.prepare("CREATE TABLE IF NOT EXISTS posts (postid INTEGER PRIMARY KEY, userid INTEGER, groupid INTEGER, created_at INTEGER, textcontent TEXT, hasImages INTEGER, FOREIGN KEY(userid) REFERENCES users(userid))").run();
+            this.db.prepare("CREATE TABLE IF NOT EXISTS posts (postid INTEGER PRIMARY KEY, userid INTEGER, groupid INTEGER, created_at INTEGER, textcontent TEXT, commentcount INTEGER, hasImages INTEGER, FOREIGN KEY(userid) REFERENCES users(userid))").run();
+
+            this.db.prepare("CREATE TABLE IF NOT EXISTS comments (commentid INTEGER PRIMARY KEY, parentid INTEGER, userid INTEGER, created_at INTEGER, textcontent TEXT, hasImages INTEGER, FOREIGN KEY(userid) REFERENCES users(userid), FOREIGN KEY(parentid) REFERENCES posts(postid))").run();
         }
     }
 
@@ -37,12 +39,28 @@ class AppDatabase
 
     static add_post(postid, userid, groupid, textcontent, hasimages)
     {
-        this.db.prepare("INSERT INTO posts VALUES (?, ?, ?, unixepoch('now'), ?, ?)").run(postid, userid, groupid, textcontent, Number(hasimages));
+        this.db.prepare("INSERT INTO posts VALUES (?, ?, ?, unixepoch('now'), ?, 0, ?)").run(postid, userid, groupid, textcontent, Number(hasimages));
     }
 
     static get_posts(groupid, latest, earliest=0)
     {
         return this.db.prepare("SELECT p.*, u.username FROM posts p inner join users u on p.userid = u.userid WHERE created_at<? AND created_at>=? AND groupid=? ORDER BY created_at DESC LIMIT 100 ").all(latest, earliest, groupid);
+    }
+
+    static post_exists(postid)
+    {
+        return this.db.prepare("SELECT exists(SELECT 1 FROM posts WHERE postid = ?) AS row_exists;").get(postid)['row_exists'];
+    }
+
+    static add_comment(commentid, parentid, userid, textcontent, hasImages)
+    {
+        this.db.prepare("INSERT INTO comments VALUES (?, ?, ?, unixepoch('now'), ?, ?)").run(commentid, parentid, userid, textcontent, Number(hasImages));
+        this.db.prepare("UPDATE posts SET commentcount = commentcount + 1 WHERE postid = ?").run(parentid);
+    }
+
+    static get_comments(parentid)
+    {   
+        return this.db.prepare("SELECT c.*, u.username FROM comments c inner join users u on c.userid = u.userid WHERE parentid=? ORDER BY created_at DESC LIMIT 100 ").all(parentid);
     }
 
     /**
@@ -88,6 +106,11 @@ class AppDatabase
     static get last_postid()
     {
         return this.db.prepare("SELECT MAX(postid) FROM posts LIMIT 1;").get()['MAX(postid)']
+    }
+
+    static get last_commentid()
+    {
+        return this.db.prepare("SELECT MAX(commentid) FROM comments LIMIT 1;").get()['MAX(postid)']
     }
 
 }
