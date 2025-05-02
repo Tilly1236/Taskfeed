a4cd34d845ca48b560b3f43a1ff0255e536bcbd6
Tilly1236 <151090502+Tilly1236@users.noreply.github.com>
2025-05-02 17:24:12 -0500

    added .jxs

--- tests/AddPostPage.test.jsx
+++ tests/AddPostPage.test.jsx
@@ -1,4 +1,4 @@
-// tests/AddPostPage.test.js
+// tests/AddPostPage.test.jsx
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, test, beforeEach, jest } from '@jest/globals';
